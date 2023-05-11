d10b29ca9ef75216b160bdfc69e9176e7f62c7c1
Raghavendra Prasad <raghavendra.prasad@dltapps.co.uk>
2023-05-11 10:03:36 +0530

    Update operation

--- src/main/java/com/finops/sbiservice/controller/AccountController.java
+++ src/main/java/com/finops/sbiservice/controller/AccountController.java
@@ -1,12 +1,15 @@
 package com.finops.sbiservice.controller;
 
 import com.finops.sbiservice.domain.Account;
-import com.finops.sbiservice.domain.Address;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.FindAndModifyOptions;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class AccountController {
@@ -18,16 +21,224 @@ public class AccountController {
     @PostMapping("/create-account")
     public Account createAccount(@RequestBody Account account){
 
-        Account createdAccount = mongoTemplate.save(account, "Accounts");
+        Account createdAccount = mongoTemplate.save(account, "Accounts"); //Create operation
         return createdAccount;
 
     }
 
-    @PostMapping("/create-address")
-    public String createAddress(@RequestBody Address address){
+//    50100107290312
 
 
-        return "createdAccount";
+    @GetMapping("/getAccountByAadharNumber")
+    public List<Account> getAccountByAadharNumber(@RequestParam String aadharNumber){
+
+        Criteria criteria = Criteria.where("aadharNumber").is(aadharNumber);
+
+        Query query = new Query(criteria);
+
+        List<Account> accounts = mongoTemplate.find(query, Account.class, "Accounts"); //Read
+
+        return accounts;
+    }
+
+    @GetMapping("/getOneAccountByAadharNumber")
+    public Account getOneAccountByAadharNumber(@RequestParam String aadharNumber){
+        Criteria criteria = Criteria.where("aadharNumber").is(aadharNumber);
+        Query query = new Query(criteria);
+        Account account = mongoTemplate.findOne(query, Account.class, "Accounts"); // Read
+        return account;
+    }
+
+    @GetMapping("/getOneAccountByMobileNumber")
+    public Account getOneAccountByMobileNumber(@RequestParam String mobileNumber){
+        Criteria criteria = Criteria.where("mobileNumber").is(mobileNumber);
+        Query query = new Query(criteria);
+        Account account = mongoTemplate.findOne(query, Account.class, "Accounts"); // Read
+        return account;
+    }
+
+    @GetMapping("/getClosingBalanceByAadharNumber")
+    public double getClosingBalanceByAadharNumber(@RequestParam String aadharNumber){
+        Criteria criteria = Criteria.where("aadharNumber").is(aadharNumber);
+        Query query = new Query(criteria);
+        Account account = mongoTemplate.findOne(query, Account.class, "Accounts"); // Read
+        return account.getClosingBalance();
 
     }
+
+
+    @GetMapping("/getClosingBalanceByMobileNumber")
+    public double getClosingBalanceByMobileNumber(@RequestParam String mobileNumber){
+        Criteria criteria = Criteria.where("mobileNumber").is(mobileNumber);
+        Query query = new Query(criteria);
+        Account account = mongoTemplate.findOne(query, Account.class, "Accounts"); // Read
+        return account.getClosingBalance();
+
+    }
+
+    @PostMapping("/update-account")
+    public Account updateAccountNumber(@RequestParam String aadharNumber, @RequestParam String mobileNumber){
+
+        Criteria criteria = Criteria.where("aadharNumber").is(aadharNumber);
+        Query query = new Query(criteria);
+        query.fields().exclude("_id");
+
+        Update update = new Update();
+        update.set("mobileNumber", mobileNumber);
+
+        Account updatedAccount = mongoTemplate.findAndModify(query,
+                update,
+                new FindAndModifyOptions().returnNew(true),
+                Account.class,
+                "Accounts");
+
+        return updatedAccount;
+
+    }
+
+
+    @PostMapping("/update-all-account")
+    public Account updateAccountInfo(@RequestParam String aadharNumber, @RequestBody Account account){
+
+        Criteria criteria = Criteria.where("aadharNumber").is(aadharNumber);
+        Query query = new Query(criteria);
+        query.fields().exclude("_id");
+
+        Update update = new Update();
+        if(!account.getFirstName().isEmpty()){
+            update.set("firstName", account.getFirstName());
+        }
+        if(!account.getLastName().isEmpty()){
+            update.set("lastName", account.getLastName());
+        }
+
+        if(account.getDateOfBirth()!=null){
+            update.set("dateOfBirth", account.getDateOfBirth());
+        }
+        if(!account.getNationality().isEmpty()){
+            update.set("nationality", account.getNationality());
+        }
+        if(!account.getMobileNumber().isEmpty()){
+            update.set("mobileNumber", account.getMobileNumber());
+        }
+        if(account.getGender()!=null){
+            update.set("gender", account.getGender());
+        }
+        if(!account.getGuardianName().isEmpty()){
+            update.set("guardianName", account.getGuardianName());
+        }
+        if(!account.getEmailId().isEmpty()){
+            update.set("emailId", account.getEmailId());
+        }
+        if(!account.getAadharNumber().isEmpty()){
+            update.set("aadharNumber", account.getAadharNumber());
+        }
+        if(!account.getNominee().isEmpty()){
+            update.set("nominee", account.getNominee());
+        }
+        if(account.getAccountType()!=null){
+            update.set("accountType", account.getAccountType());
+        }
+        if(account.isInternetBankingRequired()){
+            update.set("isInternetBankingRequired", account.isInternetBankingRequired());
+        }
+        if(!account.getPanCardNumber().isEmpty()){
+            update.set("panCardNumber", account.getPanCardNumber());
+        }
+        if(account.getOpeningBalance()!=0){
+            update.set("openingBalance", account.getOpeningBalance());
+        }
+        if(account.getClosingBalance()!=0){
+            update.set("closingBalance", account.getClosingBalance());
+        }
+
+
+
+        Account updatedAccount = mongoTemplate.findAndModify(query,
+                update,
+                new FindAndModifyOptions().returnNew(true),
+                Account.class,
+                "Accounts");
+
+        return updatedAccount;
+
+    }
+//    CRUD
+//    C--> Create
+//    R --> Read
+//    U--> Update
+//    D--> Delete
+
+    //Update Operation: 1. On what basis, we need to update,
+    //                  2. Setting the update values.
+    //                  3. Update Operation.
+
 }
+
+/*{
+        "aadharNumber": "98765432190",
+        "accountType": "CURRENT",
+        "address": {
+        "city": "Bangalore",
+        "district": "Bangalore",
+        "flatNumber": "608",
+        "landMark": "Brigade road",
+        "pinCode": "560038",
+        "state": "Karnataka",
+        "streetName": "MGROAD"
+        },
+        "branch": "MGROAD",
+        "chequeBookRequired": false,
+        "city": "Bangalore",
+        "closingBalance": 200,
+        "dateOfBirth": "2003-05-10",
+        "emailId": "sum@gmail.com",
+        "firstName": "Teja",
+        "gender": "MALE",
+        "guardianName": "Vinay",
+        "ifscCode": "HDFC0098765",
+        "internetBankingRequired": false,
+        "lastName": "Prathap",
+        "mobileNumber": "8907685432",
+        "nationality": "INDIAN",
+        "nominee": "Sid",
+        "occupation": "SELFEMPLOYED",
+        "openingBalance": 200,
+        "panCardNumber": "HGVH9876D",
+        "smsAlertsRequired": false
+        }*/
+
+
+/*
+{
+        "aadharNumber": "",
+        "accountType": "CURRENT",
+        "address": {
+        "city": "",
+        "district": "",
+        "flatNumber": "",
+        "landMark": "",
+        "pinCode": "",
+        "state": "",
+        "streetName": ""
+        },
+        "branch": "",
+        "chequeBookRequired": true,
+        "city": "string",
+        "closingBalance": 0,
+        "dateOfBirth": "2001-05-11",
+        "emailId": "",
+        "firstName": "",
+        "gender": "",
+        "guardianName": "",
+        "ifscCode": "",
+        "internetBankingRequired": true,
+        "lastName": "",
+        "mobileNumber": "",
+        "nationality": "",
+        "nominee": "",
+        "occupation": "GOVTEMPLOYEE",
+        "openingBalance": 0,
+        "panCardNumber": "",
+        "smsAlertsRequired": true
+        }*/
